4a637cdfa753d715a299a82d29390c3fbdfe0e1f
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-06-19 18:12:32 -0300

    New functionality: pick favorite character

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,6 +1,20 @@
 import '../styles/characters.css';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useReducer } from "react";
 import ThemeContext from '../context/ThemeContext';
+
+const initialState={
+    favs:[]
+}
+const favReducer = (state,action)=>{
+    switch(action.type){
+        case 'ADD':
+            return{
+                favs:[...state.favs,action.payload]
+            };
+        default:
+            return state
+    }
+}
 
 const Characters = () =>{
     const [characters, setCharacters] = useState([]);
@@ -10,6 +24,13 @@ const Characters = () =>{
         .then(data=>setCharacters(data.results))
     })
     const theme = useContext(ThemeContext)
+
+    const [state,dispatch]=useReducer(favReducer,initialState);
+
+    const handleClick=(newFav) =>{
+        dispatch({type:'ADD', payload:newFav})
+    }
+
     return(
         <div className={theme?'dark-mode':'light-mode'}>
             <div className='img-cont'>{characters.map(character =>{
@@ -17,9 +38,18 @@ const Characters = () =>{
                 <div className="img-cont__img" key={character.id}>
                     <img src={character.image} alt="" />
                     <p className='img-cont__character-name'>{character.name}</p>
+                    <button className='img-cont__button' type='button' onClick={()=>handleClick(character)}>Add to Fav.</button>
                 </div>
             )
             })}</div>
+            <div>
+                <h2>Favorites</h2>
+                {state.favs.map(pers=>{
+                    return(
+                        <p key={pers.id}>{pers.name}</p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
