36d8eb9094a3145d45f410501b455c2090d9b8e7
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-06-19 20:50:54 -0300

    New functionality: remove favorite character

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -8,14 +8,20 @@ const initialState={
 const favReducer = (state,action)=>{
     switch(action.type){
         case 'ADD':
-            return{
+            return{  
+                ...state,            
                 favs:[...state.favs,action.payload]
-            };
+            }
+            ;
+        case 'REM':
+            return{
+                favs: state.favs.filter ( items => items.id !== action.payload) 
+            }
+            ;
         default:
             return state
     }
 }
-
 const Characters = () =>{
     const [characters, setCharacters] = useState([]);
     useEffect(() => {
@@ -28,9 +34,20 @@ const Characters = () =>{
     const [state,dispatch]=useReducer(favReducer,initialState);
 
     const handleClick=(newFav) =>{
-        dispatch({type:'ADD', payload:newFav})
+        let search = false
+        for(let i = 0;(i<state.favs.length);i++){
+            if(state.favs[i].id===newFav.id){
+                search = true;
+            }
+        }
+        if(search===false){
+            dispatch({type:'ADD', payload:newFav})
+        }  
+        console.log(state.favs)
     }
-
+    const handleClickRem = (delFav)=>{
+        dispatch({type:'REM', payload:delFav})
+    }
     return(
         <div className={theme?'dark-mode':'light-mode'}>
             <div className='img-cont'>{characters.map(character =>{
@@ -38,7 +55,8 @@ const Characters = () =>{
                 <div className="img-cont__img" key={character.id}>
                     <img src={character.image} alt="" />
                     <p className='img-cont__character-name'>{character.name}</p>
-                    <button className='img-cont__button' type='button' onClick={()=>handleClick(character)}>Add to Fav.</button>
+                    <button  type='button' onClick={()=>handleClick(character)}>Add to Fav.</button>
+                    <button className='img-cont__button' type='button' onClick={()=>handleClickRem(character.id)}>Remove from Fav.</button>
                 </div>
             )
             })}</div>
@@ -46,7 +64,7 @@ const Characters = () =>{
                 <h2>Favorites</h2>
                 {state.favs.map(pers=>{
                     return(
-                        <p key={pers.id}>{pers.name}</p>
+                        <p key={pers.id}>{pers.name} {pers.gender}</p>
                     )
                 })}
             </div>
